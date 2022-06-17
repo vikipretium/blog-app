6988ba24cbe2b96d1f54f1549de57cf6f88f083c
vikipretium <vighneshpasupathy@gmail.com>
2022-06-17 15:50:15 +0530

    add post methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :users
+  has_many :comments
+  has_many :likes
+  after_save :update_post_counter
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recents_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
