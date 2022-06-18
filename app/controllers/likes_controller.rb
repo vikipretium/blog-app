bec284d9186bbb4e51139f4d64962b64d52e3f29
vikipretium <vighneshpasupathy@gmail.com>
2022-06-19 00:16:38 +0530

    add error messages for methods in controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,25 @@ class LikesController < ApplicationController
   def create
     user = current_user
     post = current_post
-    post.likes.where("author_id = #{user.id}").length.positive? &&
+    post.likes.where("author_id = #{user.id}").length.positive? && (
+      flash[:error] = 'Error: you already liked it.'
+      where_to_redirect(post)
       return
+    )
     like = Like.new(author: user, post:)
-    return unless like.save
-
-    if params[:distination] == 'index'
-      redirect_back_or_to user_posts_url
-    else
-      redirect_to new_user_post_like_url(id: post.id)
+    unless like.save 
+      flash[:error] = 'Error: Like could not be saved'
+      where_to_redirect(post)
+      return
     end
+    where_to_redirect(post)
   end
+  private
+  def where_to_redirect(post)
+     if params[:distination] == 'index'
+       redirect_back_or_to user_posts_url
+     else
+       redirect_back_or_to user_post_url(id: post.id)
+     end
+  end 
 end
