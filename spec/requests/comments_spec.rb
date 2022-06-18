ae6763599f138869e19fa503ad6a20036f76cf32
vikipretium <vighneshpasupathy@gmail.com>
2022-06-19 00:24:05 +0530

    test for comment model

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    user = User.new(name:'Saied',photo:'https://unsplash.com/photos/F_-0BxGuVvo',bio:'developer')
+  post = Post.new(author:user,title:'title',text:'text')
+  subject { Comment.new(author:user,post:post,text:'comment text')}
+  before {subject}
+
+  it "author is nil" do
+     test_case = subject
+     test_case.author = nil
+     expect(test_case).to_not be_valid
+  end
+
+  it "post is nil" do
+     test_case = subject
+     test_case.post = nil
+     expect(test_case).to_not be_valid
+  end
+
+  it "text is nil or blank" do
+     test_case = subject
+     test_case.text = nil
+     expect(test_case).to_not be_valid
+     test_case.text = ""
+     expect(test_case).to_not be_valid
+  end
+  it "update comments counter" do
+    expect(post.comments_counter).to eq(0)
+     subject.save # update_comments_counter_for_post method will run after saving the comment
+     expect(post.comments_counter).to eq(1)
+  end
   end
 end
