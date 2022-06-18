5bbbcb3cdac5e04f777b577bdee599216c2fcafb
vikipretium <vighneshpasupathy@gmail.com>
2022-06-18 18:58:28 +0530

    add view for new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,13 @@ class PostsController < ApplicationController
     @user = all_posts
     @posts = @user.posts.order('id asc')
   end
-
   def show
     @post = current_post
   end
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end 
 end
