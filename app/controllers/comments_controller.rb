851f08d1056b535fb7513cbdc1c88834abe55c1a
vikipretium <vighneshpasupathy@gmail.com>
2022-06-18 21:00:35 +0530

    fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,28 @@
 class CommentsController < ApplicationController
-    def new
-       respond_to do |format|
-         format.html { render :new, locals: { comment: Comment.new } }
-       end
-     end
-     def create
-       user = current_user
-       post = current_post
-       comment = Comment.new(comment_params)
-       comment.author = user
-       comment.post = post
-         if comment.save
-           flash[:success] = "Comment saved successfully"
-           redirect_to user_post_url(id:post.id)
-         else
-           flash[:error] = "Error: Comment could not be saved"
-           redirect_to new_user_post_comment_url
-       end 
-     end 
-     private
+  def new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: Comment.new } }
+    end
+  end
 
-     def comment_params
-       params.require(:comment).permit(:text)
-     end
-   end
+  def create
+    user = current_user
+    post = current_post
+    comment = Comment.new(comment_params)
+    comment.author = user
+    comment.post = post
+    if comment.save
+      flash[:success] = "Comment saved successfully"
+      redirect_to user_post_url(id: post.id)
+    else
+      flash[:error] = "Error: Comment could not be saved"
+      redirect_to new_user_post_comment_url
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
