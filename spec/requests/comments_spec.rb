eda591a42c5d304ebede1476b16a06b5f8aa07e0
vikipretium <vighneshpasupathy@gmail.com>
2022-06-19 00:44:26 +0530

    add comment model

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,34 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
   describe 'GET /index' do
-    user = User.new(name:'Saied',photo:'https://unsplash.com/photos/F_-0BxGuVvo',bio:'developer')
-  post = Post.new(author:user,title:'title',text:'text')
-  subject { Comment.new(author:user,post:post,text:'comment text')}
-  before {subject}
-
-  it "author is nil" do
-     test_case = subject
-     test_case.author = nil
-     expect(test_case).to_not be_valid
-  end
-
-  it "post is nil" do
-     test_case = subject
-     test_case.post = nil
-     expect(test_case).to_not be_valid
-  end
-
-  it "text is nil or blank" do
-     test_case = subject
-     test_case.text = nil
-     expect(test_case).to_not be_valid
-     test_case.text = ""
-     expect(test_case).to_not be_valid
-  end
-  it "update comments counter" do
-    expect(post.comments_counter).to eq(0)
-     subject.save # update_comments_counter_for_post method will run after saving the comment
-     expect(post.comments_counter).to eq(1)
-  end
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end
