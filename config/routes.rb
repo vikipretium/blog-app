13ff8b239a88a28aa7254b800c0f105d56643763
nevisende <ekonomitakipmail@gmail.com>
2022-06-20 23:49:40 +0300

    Setup: initialize the devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
+  devise_scope :user do
+    authenticated :user do
+      root :to => "users#show", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new,:create,:show] do
       resources :comments, only: [:new, :create]
